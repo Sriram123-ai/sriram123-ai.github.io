77a5d51316c9f58745dcda81c0c2b9f5091a92fc
sriram eagala <25P35A4237@acet.ac.in>
2026-06-07 12:46:15 +0530

    Delete career.js

--- career.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitApplication, getApplications } = require('../controllers/careerController');
-
-router.post('/', submitApplication);
-router.get('/', getApplications);
-
-module.exports = router;
