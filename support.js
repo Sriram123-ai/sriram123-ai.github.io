6fb78a05685bda29928d719501374c4a8ae1574f
sriram eagala <25P35A4237@acet.ac.in>
2026-06-07 12:47:09 +0530

    Delete support.js

--- support.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitSupport, getTickets } = require('../controllers/supportController');
-
-router.post('/', submitSupport);
-router.get('/', getTickets);
-
-module.exports = router;
